1dfd15b4216f4b3f3db887fd95a02911f066c08f
Mack Pearson <76492881+chopov1@users.noreply.github.com>
2023-06-03 20:40:51 -0500

    added healthbars to enemies

--- JamJamUnityProj/Assets/Scripts/EnemyTest.cs
+++ JamJamUnityProj/Assets/Scripts/EnemyTest.cs
@@ -1,21 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class EnemyTest : Mob
 {
     //Vector3 playerPos;
-    [SerializeField] float speed;
+    [SerializeField] float speed, maxHealth;
+
+    float health;
+    public float basePlayerDmg;
+
+    Slider healthBar;
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<Slider>();
+        health = maxHealth;
+        SetHealthUI();
+    }
     void Update()
     {
         Vector2 dir = (playerPosition - transform.position).normalized;
         transform.Translate(dir * speed * Time.deltaTime);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
+
+    private void OnCollisionStay2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "PlayerWeapon")
         {
-            this.gameObject.SetActive(false);
+            HurtMob(basePlayerDmg);
+            if (health <= 0)
+            {
+                mobDeath();
+            }
         }
+    }
+
+    void HurtMob(float damageAmount)
+    {
+        health -= damageAmount;
+        SetHealthUI();
+    }
+
+    void mobDeath()
+    {
+        this.gameObject.SetActive(false);
+        health = maxHealth;
+        SetHealthUI();
+    }
+
+    void SetHealthUI()
+    {
+        healthBar.value = health;
     }
 }
