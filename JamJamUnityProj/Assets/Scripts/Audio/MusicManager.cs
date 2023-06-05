ac89074009f52702e38b1018162ab32ed34f64f9
Ricky Bakersmith <90644027+RickyB923@users.noreply.github.com>
2023-06-05 14:36:29 -0500

    Music bug fix

--- JamJamUnityProj/Assets/Scripts/Audio/MusicManager.cs
+++ JamJamUnityProj/Assets/Scripts/Audio/MusicManager.cs
@@ -27,7 +27,25 @@ public class MusicManager : MonoBehaviour
     {
         //playing around with shit, eventually Ill match music to timer or vis versa so clips end as round ends and trigger when player starts round
         musicAS.volume = 0.5f;
-        musicAS.pitch = 1 - ((gameManager.waveNumber - 1) * gameManager.waveLengthIncrease/60);
+        switch(gameManager.waveNumber)
+        {
+            case 1:
+                musicAS.pitch = 1;
+                break;
+            case 2:
+                musicAS.pitch = 0.75f;
+                break;
+            case 3:
+                musicAS.pitch = 0.5f;
+                break;
+            case 4:
+                musicAS.pitch = 0.25f;
+                break;
+            default:
+                musicAS.pitch = 1;
+                break;
+        }
+        //musicAS.pitch = 1 - ((gameManager.waveNumber - 1) * gameManager.waveLengthIncrease/60);
         musicAS.clip = level1Music;
         musicAS.Play();
     }
