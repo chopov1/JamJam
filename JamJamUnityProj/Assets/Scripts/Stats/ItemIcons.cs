12a67a3005f634bd3c5376686d67a12ddf07a59a
Lane Howard <lhoward2@colum.edu>
2023-06-05 02:36:41 -0500

    added item icons and fixed ui

---
+++ JamJamUnityProj/Assets/Scripts/Stats/ItemIcons.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class ItemIcons : ScriptableObject
+{
+    public List<Sprite> positiveNouns;
+    public List<Sprite> negativeNouns;
+}
